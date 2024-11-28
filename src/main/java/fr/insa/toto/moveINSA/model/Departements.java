0b55aad377a4fd95efd0db46b7fd0741f1f7bc43
cleo <cleo.heitzmann@gmail.com>
2024-11-28 08:39:08 +0100

    test

--- src/main/java/fr/insa/toto/moveINSA/model/Departements.java
+++ src/main/java/fr/insa/toto/moveINSA/model/Departements.java
@@ -14,7 +14,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with CoursBeuvron.  If not, see <http://www.gnu.org/licenses/>.
+along with CoursBeuhhhhvron.  If not, see <http://www.gnu.org/licenses/>.
  */
 package fr.insa.toto.moveINSA.model;
 
